3fbd244970c5d273533bc721fe30a5cc995a665b
Denis <dentik1@abv.bg>
2018-12-06 11:20:44 +0200

    1.Create database with code first - installed ef6 and created DbManagement project.

---
+++ CarsMagazine/CarsDbContext/CarsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsDbContext.Management
+{
+    public class CarsDbContext : DbContext
+    {
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
